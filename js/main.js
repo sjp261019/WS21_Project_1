3d921e04e5df3a060362eb78290a3a63835ffb08
sjp261019 <sjp261019@gmail.com>
2022-01-18 06:36:16 -0500

    01/18/22 - 4

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
         $(".lyrics").hide("slow");
         $(".typing").show("slow");
         $(".lyric3").delay(4500).show("slow");
+
         $(this).animate({"opacity":"0"}, 800, "swing");
         $(this).delay(500).hide("slow");
         $("#next2").delay(8800).show("slow");
@@ -27,7 +28,13 @@ $(document).ready(function(){
     $("#next2").on("click", function(){
         $(".typing").hide("slow");
         $(".lyric3").delay(700).hide("slow");
+        $("#slider2").delay(1200).fadeIn("slow");
+        $(".lyric4").delay(1500).fadeOut("slow");
+        $("#slider3").delay(2000).fadeIn("slow");
+        $("#slider4").delay(2500).fadeIn("slow");
+        
         $(this).animate({"opacity":"0"}, 800, "swing");
         $(this).delay(500).hide("slow");
+        $("#next3").delay(3200).fadeIn("slow");
         });
 });
